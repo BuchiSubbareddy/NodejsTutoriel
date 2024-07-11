ae399095718cec7daa7f496561665240c1a2bc45
BuchiSubbareddy <subbareddy.411@gmail.com>
2024-07-11 16:13:30 +0530

    syn and asyn in nodejs with example

--- index.js
+++ index.js
@@ -9,19 +9,70 @@
 // }
 // greetViswas(greet);
 
-const eventEmitter= require("node:events")
+// const eventEmitter= require("node:events")
 
-const emitter = new eventEmitter();
+// const emitter = new eventEmitter();
 
-emitter.on("order-pizza",(size,topping)=>{
+// emitter.on("order-pizza",(size,topping)=>{
 
-    console.log(`order recived with ${size} and with ${topping}`)
-});
+//     console.log(`order recived with ${size} and with ${topping}`)
+// });
 
-emitter.on("order-pizza",(size)=>{
-    if(size==="large"){
-        console.log("give cooldrink");
+// emitter.on("order-pizza",(size)=>{
+//     if(size==="large"){
+//         console.log("give cooldrink");
+//     }
+// })
+// console.log("hi subbu");
+// emitter.emit("order-pizza","large","bujjii");
+
+
+// const buffer = new Buffer.from("subbu");
+// console.log(buffer.toJSON());
+
+
+// Asyncronus java script example 
+
+
+const fs = require("node:fs")
+
+console.log("first")
+const fileContent = fs.readFileSync("./file.txt","utf-8");
+
+console.log(fileContent);
+
+console.log("second")
+const subbu =fs.readFile("./file.txt","utf-8",(error, data)=>{
+    if(error){
+        console.log(error)
+    } else {
+        console.log(data)
     }
 })
-console.log("hi subbu");
-emitter.emit("order-pizza","large","bujjii");
+console.log("seventh")
+console.log(subbu)
+
+console.log("third")
+fs.readFile("./file.txt","utf-8",(error, data)=>{
+    if(error){
+        console.log(error)
+    } else {
+        console.log(data)
+    }
+})
+
+console.log("fourth")
+
+const fileContent1 = fs.readFileSync("./file.txt","utf-8");
+console.log(fileContent1)
+const fileContent2 = fs.readFileSync("./file.txt","utf-8");
+console.log(fileContent2)
+
+const fileContent3 = fs.readFileSync("./file.txt","utf-8");
+console.log(fileContent3)
+const fileContent4 = fs.readFileSync("./file.txt","utf-8");
+console.log(fileContent4)
+const fileContent5 = fs.readFileSync("./file.txt","utf-8");
+console.log(fileContent5)
+const fileContent6 = fs.readFileSync("./file.txt","utf-8");
+console.log(fileContent6)
