185804f87024fdef15edd801483282fa48861d6b
BuchiSubbareddy <subbareddy.411@gmail.com>
2024-07-12 18:07:55 +0530

    http server with node js

--- index.js
+++ index.js
@@ -113,15 +113,66 @@
 // same stream program used for pipes and added bleow line and commented readable. on
 // readable.pipe(writable);
 
+//created web server using http and connection with front end
+// const http = require("node:http")
+// const fs = require("node:fs")
+
+// const server = http.createServer((req,res)=>{
+//     let name = "subbu";
+//     res.writeHead(200,{"content-type":"text/html"});
+//     let html = fs.createReadStream("./index.html","utf-8");
+//     html = html.replace("{{name}}",name);
+//     res.end(html);
+// });
+
+// server.listen(3000,()=>{
+//     console.log("server created successfully on port 300")
+// });
+
+// const http = require("node:http");
+// const fs = require("node:fs");
+
+// const server = http.createServer((req, res) => {
+//     let name = "subbu";
+//     res.writeHead(200, { "Content-Type": "text/html" });
+
+//     fs.readFile("./index.html", "utf-8", (err, html) => {
+//         if (err) {
+//             res.writeHead(500, { "Content-Type": "text/plain" });
+//             //res.end("Internal Server Error");
+//             return;
+//         }
+
+//         // Replace placeholder with the actual name
+//         html = html.replace("{{name}}", name);
+
+//         // Send the response with the modified HTML
+//         res.end(html);
+//     });
+// });
+
+// server.listen(3000, () => {
+//     console.log("Server created successfully on port 3000");
+// });
+
 
 const http = require("node:http")
+const fs = require("node:fs")
 
 const server = http.createServer((req,res)=>{
-    //res.writeHead(200);
-    res.end("hello web server");
+  if (req.url==="/"){
+    res.writeHead(200,{"content-type":"text/html"});
+    res.end("home page");
+  }else if (req.url="/about"){
+    res.writeHead(200,{"content-type":"text/html"});
+    res.end("about page");
+  } else{
+    res.writeHead(404);
+    res.end("page not found");
+  }
+    
 });
 
 server.listen(3000,()=>{
     console.log("server created successfully on port 300")
 });
-
